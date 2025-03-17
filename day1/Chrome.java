a2938f3cd2f64f6d825c705ad7d29629796b5765
RathiRamachandran <rathiramachandran20@gmail.com>
2025-03-17 21:46:33 +0530

    Add files via upload
    Day 1

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package Week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		//float
+		float versionOfBrowser=91.0F;
+		System.out.println(versionOfBrowser);
+		
+		//String
+				String searchengine ="Google";
+				System.out.println(searchengine);
+		
+		//Boolean
+		boolean isFirebox=false;
+		System.out.println(isFirebox);
+		
+		//int
+		int releaseYear =2008;
+		System.out.println(releaseYear);
+		
+		//char
+				char shortcutOfChrome='C';
+				System.out.println(shortcutOfChrome);
+	}
+
+}
